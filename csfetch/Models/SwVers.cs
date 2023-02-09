15af8afe6f7c55d77364de41c9cc74788adae2ec
Bill Allaire <mytec@me.com>
2023-02-08 21:54:52 -0500

    Initial commit

---
+++ csfetch/Models/SwVers.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CliStatus
+{
+	public class SwVers
+	{
+		public string? ProductName { get; private set; } = "";
+		public string? ProductVersion { get; private set; } = "";
+		public string? BuildVersion { get; private set; } = "";
+
+        private string swVersOutput = "";
+
+		public SwVers()
+		{
+			FetchSwVers();
+            ParseSwVers();
+		}
+
+		private void FetchSwVers()
+		{
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "sw_vers",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                }
+            };
+
+            p.Start();
+            swVersOutput = p.StandardOutput.ReadToEnd() ?? "";
+            p.WaitForExit();
+            p.Dispose();
+        }
+
+        private void ParseSwVers()
+        {
+            var parts = swVersOutput.Split("\n");
+            if (parts.Length == 4)
+            {
+                string[] lineParts;
+                foreach (string line in parts)
+                {
+                    lineParts = line.Split(":");
+                    if (lineParts.Length == 2)
+                    {
+                        switch (lineParts[0].ToLower())
+                        {
+                            case "productname":
+                                ProductName = lineParts[1].Trim();
+                                break;
+                            case "productversion":
+                                ProductVersion = lineParts[1].Trim();
+                                break;
+                            case "buildversion":
+                                BuildVersion = lineParts[1].Trim(); ;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"OS: {ProductName} {ProductVersion} {BuildVersion}";
+        }
+	}
+}
+
---
+++ csfetch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace CliStatus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UserHostName());
+            Console.WriteLine(UserHostSpacer());
+
+            var swVers = new SwVers();
+            Console.WriteLine(swVers.ToString());
+            Console.WriteLine(Uname());
+
+            // Environment.GetEnvironmentVariables
+        }
+
+        private static string UserHostName()
+        {
+            return $"{Environment.UserName}@{Environment.MachineName}";
+        }
+
+        private static string UserHostSpacer()
+        {
+            // TODO: Should be some default or width of UserHostName, whichever
+            // is shorter
+            return "------------------------------";
+        }
+
+        private static string Uname()
+        {
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "uname",
+                    Arguments = "-mrs",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                }
+            };
+
+            p.Start();
+            string? inputText = p.StandardOutput.ReadToEnd() ?? "";
+            p.WaitForExit();
+            p.Dispose();
+
+            return inputText;
+        }
+    }
+}
